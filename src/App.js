b42dcf83cc3f6f9177f702d88678e36cf1e6f416
andxbes <andxbes@gmail.com>
2025-01-25 21:01:53 +0200

    355

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '', element: <Home /> },
+      { index: true, element: <Home /> },
       { path: 'products', element: <Products /> },
       { path: 'products/:productId', element: <ProductDetails /> }
     ],
